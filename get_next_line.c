22cb9e57e334912c595a892f0e243f6db5e3d993
Vladyslav Kostenko <vkostenk@e1z2r3p39.42.us.org>
2018-07-24 12:48:28 -0700

    fdg

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,18 @@
 /*   By: vkostenk <vkostenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 12:27:55 by vkostenk          #+#    #+#             */
-/*   Updated: 2018/07/24 12:45:09 by vkostenk         ###   ########.fr       */
+/*   Updated: 2018/07/24 12:48:11 by vkostenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "get_next_line.h"
 
  int get_next_line(const int fd, char **line)
  {
    file = open(filename, O_RDONLY);
    if (file < 0)
    return (NULL);
-   while (read(file, &buf, 21))
+   while (read(file, &buf, BUFF_SIZE))
    {
      ft_bzero(buf, 21);
    }
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: vkostenk <vkostenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 12:28:08 by vkostenk          #+#    #+#             */
-/*   Updated: 2018/07/24 12:44:05 by vkostenk         ###   ########.fr       */
+/*   Updated: 2018/07/24 12:46:59 by vkostenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
+# define BUFF_SIZE 32
 
 int get_next_line(const int fd, char **line);
 
