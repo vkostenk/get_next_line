f1a02de29947a9f3ba18a457a400d5fa9e69711c
Vladyslav Kostenko <vkostenk@e1z2r3p39.42.us.org>
2018-07-24 12:46:02 -0700

    asa

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,18 @@
 /*   By: vkostenk <vkostenk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 12:27:55 by vkostenk          #+#    #+#             */
-/*   Updated: 2018/07/24 12:29:22 by vkostenk         ###   ########.fr       */
+/*   Updated: 2018/07/24 12:45:09 by vkostenk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
  int get_next_line(const int fd, char **line)
  {
-   
+   file = open(filename, O_RDONLY);
+   if (file < 0)
+   return (NULL);
+   while (read(file, &buf, 21))
+   {
+     ft_bzero(buf, 21);
+   }
+   close(file);
  }
